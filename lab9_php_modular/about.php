d9fdc949db14221fae023fd9ecfd385cb72d52fd
Alanda04 <52759649+Alanda04@users.noreply.github.com>
2021-06-06 13:55:15 +0700

    Add files via upload

---
+++ lab9_php_modular/about.php
@@ -0,0 +1,8 @@
+<?php require('header.php'); ?>
+<div class="content">
+<h2>Ini Halaman About</h2>
+<p>Ini adalah bagian content dari halaman.</p>
+<p>Perkenalkan Nama Saya Alanda Jiwani kelas TI.19.B1.</p>
+</div>
+<?php require('footer.php'); ?>
+<?php
---
+++ lab9_php_modular/footer.php
@@ -0,0 +1,6 @@
+<footer>
+<p>&copy; 2021, Informatika, Universitas Pelita Bangsa Alanda Jiwani</p>
+</footer>
+</div>
+</body>
+</html>
